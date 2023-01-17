ffdc5d633a06d5bf267f2f76fbe7286ca1a7189d
Kame2021 <21cm0108@jec.ac.jp>
2023-01-17 11:14:37 +0900

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Ryo on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
